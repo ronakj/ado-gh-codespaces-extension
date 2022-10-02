0636fa51e92863fd6a6136c3ce3eb552505bc020
Ronak Jain <ronakjain.public@gmail.com>
2022-10-02 06:30:18 +0000

    uAdd auto versioning to dev

--- createDevManifest.js
+++ createDevManifest.js
@@ -9,7 +9,16 @@ const DevExtensionId = "ADOGitHubCodespacesDev";
 const manifest = (fs.readFileSync(manifestPath).toString());
 const newManifest = JSON.parse(manifest.split(ExtensionId).join(DevExtensionId))
 
-newManifest.baseUri = "https://localhost:3000";
-newManifest.public = false;
+const devManifestData = {
+  baseUri: "https://localhost:3000",
+  public: false,
+  version: "1.0." + Date.now(),
+  name: newManifest.name + " (DEV)",
+  description: newManifest.description + " (DEV)",
+  contributions: newManifest.contributions.map(contribution => ({
+    ...contribution,
+    description: contribution.description + " (DEV)"
+  }))
+};
 
-fs.writeFileSync(newManifestPath, JSON.stringify(newManifest, null, 2));
+fs.writeFileSync(newManifestPath, JSON.stringify(Object.assign({}, newManifest, devManifestData), null, 2));
