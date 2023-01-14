66bbca2fcd7c0385bf283a246c930c663086ca96
Ronak Jain <ronakjain.public@gmail.com>
2023-01-14 14:47:20 +0530

    Redirect to Codespace Hub

--- src/ExtensionComponents/CodespacesAction/CodespacesAction.ts
+++ src/ExtensionComponents/CodespacesAction/CodespacesAction.ts
@@ -1,9 +1,48 @@
+import {
+  CommonServiceIds,
+  ILocationService,
+  IProjectPageService,
+} from "azure-devops-extension-api";
+import {
+  GitServiceIds,
+  IVersionControlRepositoryService,
+} from "azure-devops-extension-api/Git/GitServices";
 import * as SDK from "azure-devops-extension-sdk";
 
-SDK.register("codespaces-create-on-branch", () => {
+SDK.register("codespaces-create-on-branch", async () => {
   return {
-    execute: async (context: unknown) => {
-      console.log("unknown", context);
+    execute: async (context: {
+      name: string;
+      fullName: string;
+      isFolder: boolean;
+      comment: string;
+      commitId: string;
+      isUserCreated: boolean;
+      isDefault: boolean;
+      isCompare: boolean;
+      lastUpdatedBy: string;
+      aheadCount: number;
+      behindCount: number;
+      favorite: boolean;
+    }) => {
+      const [locationService, projectService] = await Promise.all([
+        SDK.getService<ILocationService>(CommonServiceIds.LocationService),
+        SDK.getService<IProjectPageService>(
+          CommonServiceIds.ProjectPageService
+        ),
+      ]);
+      const [url, project] = await Promise.all([
+        locationService.getResourceAreaLocation(
+          "79134C72-4A58-4B42-976C-04E7115F32BF" // core resource area id
+        ),
+        projectService.getProject(),
+      ]);
+      window.open(
+        `${url + project?.name}/_apps/hub/${
+          SDK.getExtensionContext().id
+        }.codespaces-hub`,
+        "_top"
+      );
     },
   };
 });
